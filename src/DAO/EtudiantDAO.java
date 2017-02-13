a04405f66c099e33767e503104e718226b3daa73
ismailrei <ismailrei@gmail.com>
2017-02-13 17:40:18 +0000

    [IMP] update etudiantDao

--- src/DAO/EtudiantDAO.java
+++ src/DAO/EtudiantDAO.java
@@ -6,6 +6,7 @@ import Entities.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+
 import java.util.*;
 
 /**
@@ -18,67 +19,60 @@ public class EtudiantDAO {
 
     public EtudiantDAO() {
         this.session = HibernateUtil.getSession();
-        this.transaction =  session.getTransaction();
+        this.transaction = session.getTransaction();
     }
 
-    public void save( Etudiant etudiant ) {
+    public void save(Etudiant etudiant) {
         this.transaction.begin();
-        this.session.saveOrUpdate( etudiant );
+        this.session.saveOrUpdate(etudiant);
         this.transaction.commit();
     }
 
     public List<Etudiant> listAll() {
-        return  this.session.createQuery( "from Etudiant" ).list();
+        return this.session.createQuery("from Etudiant").list();
     }
 
-    public Etudiant getEtudiant(String cne ) {
-        return ( Etudiant ) session.get( Etudiant.class, cne );
+    public Etudiant getEtudiant(String cne) {
+        return (Etudiant) session.get(Etudiant.class, cne);
     }
 
-    public void delete( String cne ) {
+    public void delete(String cne) {
         this.transaction.begin();
 
-        Etudiant etudiant = ( Etudiant ) session.load( Etudiant.class, cne );
-        this.session.delete( etudiant );
+        Etudiant etudiant = (Etudiant) session.load(Etudiant.class, cne);
+        this.session.delete(etudiant);
 
         this.transaction.commit();
     }
 
-    public void importSansNote( List<Etudiant> liste ) {
+    public void importSansNote(Etudiant E) {
         Email email = new Email();
         UserDAO userDao = new UserDAO();
-        User u = new User();
+        if( userDao.getByLogin(E.getEmail()) ==  null ) {
 
-        for( Etudiant E : liste ) {
+            User u = new User();
 
             /* Creating an acount for student. */
-            u.setAdmin( false );
-            u.setPassword(E.getCne());
-            u.setEmail( E.getEmail());
-            u.setLogin( E.getEmail());
-            userDao.save( u );
+            u.setAdmin(false);
+            u.setPassword(E.getCne()+ ""+ new Random().nextInt(100));
+            u.setEmail(E.getEmail());
+            u.setLogin(E.getEmail());
+            userDao.save(u);
 
-            /* Save Student. */
-            this.save( E );
+        }
+        /* Save Student. */
+        E.setOneToOne(userDao.getByLogin(E.getEmail()));
+        this.save(E);
 
             /* Send him his Credentials. */
-            email.setFrom( "oriented.ensat@gmail.com" );
-            email.setPassword( "Ensat@01/01/1998" );
-            email.setSubject( "Données de connexion" );
-            email.setBody( "login: "+E.getOneToOne().getLogin() + "<br/> Mot de passe" +E.getOneToOne().getPassword() );
-            email.setTo( E.getEmail());
-            email.sendMail();
-        }
+        email.setFrom("oriented.ensat@gmail.com");
+        email.setPassword("Ensat@01/01/1998");
+        email.setSubject("Données de connexion");
+        email.setBody("login: " + E.getOneToOne().getLogin() + " Mot de passe: " + E.getOneToOne().getPassword());
+        email.setTo(E.getEmail());
+        email.sendMail();
 
-    }
 
-    public void importNote( List<Etudiant> liste ) {
-        Etudiant oldEtudiant;
-        for( Etudiant E : liste ){
-            oldEtudiant = this.getEtudiant( E.getCne() );
-            oldEtudiant.setNote( E.getNote());
-            this.save( oldEtudiant );
-        }
     }
 
     /* Returns a list of students sorted by note */
@@ -86,60 +80,60 @@ public class EtudiantDAO {
         String hql = "FROM Etudiant E  ORDER BY E.note DESC";
         Query query = session.createQuery(hql);
         List results = query.list();
-        return  results;
+        return results;
     }
 
     public void adjustOrder() {
         int ranking = 1;
-        for( Etudiant E : listByNote()) {
-            E.setClassement( ranking );
-            this.save( E );
-            ranking ++;
+        for (Etudiant E : listByNote()) {
+            E.setClassement(ranking);
+            this.save(E);
+            ranking++;
         }
     }
 
     /* Takes a string of tokens delimited by / and return the corresponding array. */
-    public ArrayList< String> tokens( String string ) {
-        ArrayList< String > tokens = new ArrayList<>();
-        StringTokenizer tokenized = new StringTokenizer( string,"/" );
+    public ArrayList<String> tokens(String string) {
+        ArrayList<String> tokens = new ArrayList<>();
+        StringTokenizer tokenized = new StringTokenizer(string, "/");
 
-        while ( tokenized.hasMoreTokens())
-            tokens.add( tokenized.nextToken());
-        return  tokens;
+        while (tokenized.hasMoreTokens())
+            tokens.add(tokenized.nextToken());
+        return tokens;
     }
 
-    public void processAffected( ) {
+    public void processAffected() {
         /* Get the Promo */
         Calendar now = Calendar.getInstance();
         int year = now.get(Calendar.YEAR);
 
         /* Initializing 'effectif' count. */
         FiliereDAO filiereDAO = new FiliereDAO();
-        HashMap< String, Integer > effectif =  new HashMap<>();
-        for( Filiere filiere : filiereDAO.listAll())
-            effectif.put( filiere.getId() ,0 );
+        HashMap<String, Integer> effectif = new HashMap<>();
+        for (Filiere filiere : filiereDAO.listAll())
+            effectif.put(filiere.getId(), 0);
 
         /* Sort Students by 'note'. */
-        List< Etudiant > Etudiants = this.listByNote();
+        List<Etudiant> Etudiants = this.listByNote();
 
-        for( Etudiant E : Etudiants ) {
+        for (Etudiant E : Etudiants) {
 
             /* Set the Promo */
-            E.setAnnee(""+year);
+            E.setAnnee("" + year);
 
             /* Transforming 'choix' from a tokenized String into an Array of String. */
-            ArrayList< String > choix = this.tokens( E.getChoix());
+            ArrayList<String> choix = this.tokens(E.getChoix());
 
-            for( String choice : choix ) {
+            for (String choice : choix) {
 
                 /* The chosen 'filiere' is not full yet. */
-                if( effectif.get( choice ) < filiereDAO.getFiliere( choice ).getEffictif()) {
+                if (effectif.get(choice) < filiereDAO.getFiliere(choice).getEffictif()) {
                     /* A hardWorker give him/her what he/she desires ;) */
-                    E.setAffected( filiereDAO.getFiliere( choice ));
-                    this.save( E );
+                    E.setAffected(filiereDAO.getFiliere(choice));
+                    this.save(E);
 
                     /* Increment the 'effectif' for the current 'filiere'. */
-                    effectif.put( choice, effectif.get( choice) + 1 );
+                    effectif.put(choice, effectif.get(choice) + 1);
 
 
                     /* We should break this loop and prevent it from changing the 'affected', since the choice is made. */
@@ -151,10 +145,11 @@ public class EtudiantDAO {
             }
         }
     }
-    public void deleteAll(  ) {
 
-        for( Etudiant E : this.listAll())
-            this.delete( E.getCne() );
+    public void deleteAll() {
+
+        for (Etudiant E : this.listAll())
+            this.delete(E.getCne());
     }
 
 }
